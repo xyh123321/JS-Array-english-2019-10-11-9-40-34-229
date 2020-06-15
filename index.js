ae29e288b4e33601c8a31069a3c4242383cc9bc4
jayden <376564838@qq.com>
2020-06-15 09:31:23 +0800

    commit codes

--- index.js
+++ index.js
@@ -2,25 +2,51 @@
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
-
+console.log(typeof a);
+console.log(typeof b);
 
 // Write a program that multiplies each entry in the following array by 2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
-
+for (let i = 0; i < a.length; i++) {
+    a[i] = a[i]*2;
+}
+console.log(a);
 
 // Write the program, according to the following requirements output results.
 var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
+console.log(colors.join(' '));
 // case 2 output: 'Red+Green+White+Black'
+console.log(colors.join('+'));
 // case 3 output: 'Red,Green,White,Black'
+console.log(colors.join(','));
 
 
 // Write a program to sort the Numbers in the following array from largest to smallest.
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
+console.log(a.sort(function(val1,val2) {
+    return val2-val1;
+}));
 
 
 // Program to find the most frequent element in the following array.
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+var res = {};
+for (let i = 0; i < a.length; i++) {
+    if (res[a[i]]) {
+        res[a[i]] ++;
+    } else {
+        res[a[i]] = 1;
+    }
+}
+var maxNum = 0;
+var maxName;
+for (let r in res) {
+    if (res[r] > maxNum) {
+        maxName = r;
+    }
+}
+console.log(maxName);
